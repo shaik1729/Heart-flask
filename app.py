616bf77f806af2d9d4bed0b777107733a0aa3ab5
shaik1729 <shaiktajuddinshavali@gmail.com>
2021-10-03 13:55:57 +0530

    updated UI

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 import numpy as np
 import pickle
 
+from werkzeug.utils import redirect
+
 
 app = Flask(__name__)
 model = pickle.load(open('Heart.pkl', 'rb'))
@@ -12,26 +14,28 @@ def Home():
 
 @app.route("/predict", methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        age = int(request.form['age'])
-        sex = int(request.form['sex'])
-        cp = int(request.form['cp'])
-        trestbps = int(request.form['trestbps'])
-        chol = int(request.form['chol'])
-        fbs = int(request.form['fbs'])
-        restecg = int(request.form['restecg'])
-        thalach = int(request.form['thalach'])
-        exang = int(request.form['exang'])
-        oldpeak = float(request.form['oldpeak'])
-        slope = int(request.form['slope'])
-        ca = int(request.form['ca'])
-        thal = int(request.form['thal'])
+    try:
+        if request.method == 'POST':
+            age = int(request.form['age'])
+            sex = int(request.form['sex'])
+            cp = int(request.form['cp'])
+            trestbps = int(request.form['trestbps'])
+            chol = int(request.form['chol'])
+            fbs = int(request.form['fbs'])
+            restecg = int(request.form['restecg'])
+            thalach = int(request.form['thalach'])
+            exang = int(request.form['exang'])
+            oldpeak = float(request.form['oldpeak'])
+            slope = int(request.form['slope'])
+            ca = int(request.form['ca'])
+            thal = int(request.form['thal'])
 
-        values = np.array([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]])
-        prediction = model.predict(values)
+            values = np.array([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]])
+            prediction = model.predict(values)
 
-        return render_template('result.html', prediction=prediction)
-
+            return render_template('result.html', prediction=prediction)
+    except:
+        return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
